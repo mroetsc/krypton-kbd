90bcf7f7247bf7b772ab840bc33492b894e48b6e
mroetsc <mail@mattes-roetschke.de>
2025-03-01 17:37:36 +0100

    Added conversion of jscad to stl to watcher script

--- ergogen/watcher.sh
+++ ergogen/watcher.sh
@@ -9,4 +9,5 @@ while true; do
   inotifywait -e modify,create,close_write "$DIRECTORY/$WATCH_FILE"
   echo "File change detected, running ergogen..."
   ergogen .
+  find output/cases -name "*.jscad" -exec npx @jscad/cli@1 {} -of stla -o {}.stl \;
 done
